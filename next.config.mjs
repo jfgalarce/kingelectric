05f5aba436c366ba8eab427f8653169f03d18bd7
jfgalarce <jfgalarce@gmail.com>
2025-03-15 23:28:28 +0000

    se hace otros cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: 'export',};
+const nextConfig = {
+  output: 'export', // Para que genere archivos estáticos compatibles
+  distDir: 'out',   // Cambia la carpeta de salida a `out`
+
+};
 
 export default nextConfig;
