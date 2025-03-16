92d3fd05796da00c2c9afd84df921df85a14190d
jfgalarce <jfgalarce@gmail.com>
2025-03-16 22:15:06 +0000

    se corrige errores

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Para que genere archivos estáticos compatibles
-  distDir: 'out',   // Cambia la carpeta de salida a `out`
-
+  output: "standalone",
 };
 
 export default nextConfig;
