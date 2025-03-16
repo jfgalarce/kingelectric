72dce2e32f354f63cc918d40f76d86b2c27cd32f
jfgalarce <jfgalarce@gmail.com>
2025-03-16 23:43:46 +0000

    correccion de errores

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: 'export', // Para que genere archivos estáticos compatibles
+  distDir: 'out',   // Cambia la carpeta de salida a `out`
+
 };
 
 export default nextConfig;
--- src/components/ui/myUI/Button.jsx
+++ src/components/ui/myUI/Button.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+
 const Button = ({children}) => {
   return (
     <Link href="/" className='flex gap-2 items-center justify-center hover:bg-slate-800 p-2 bg-red-600 rounded-lg shadow-lg text-white font-medium text-base border'>{children}</Link>
